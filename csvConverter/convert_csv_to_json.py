e37fad3701247a02cdeeaa7a2668869e0dffc1df
DHWill <will@dominicharris.studio>
2024-05-13 15:56:01 +0100

    updated csv->json parser

--- csvConverter/convert_csv_to_json.py
+++ csvConverter/convert_csv_to_json.py
@@ -1,28 +1,8 @@
 import csv
 import json
 
-def calcPosition(_parentState):
-    positions = {"Left":0, "Front":1, "Right":2, "PB":0, "BB":1}
-    isEarlyExit = False
-    if(len(_parentState.split('_')) > 1):
-        currentPosition = _parentState.split('_')[0]
-        targetPosition = _parentState.split('_')[1]
-    else:
-        currentPosition = _parentState
-        targetPosition = _parentState
-    position = positions.get(currentPosition)
-    targetPosition = positions.get(targetPosition)
-
-    return position, targetPosition
-
-def getDataFromClipName(_clipName, dictList):
-    for reg in dictList:
-        if(reg['Clip Name'] == _clipName):
-            return reg
-
-def csv_to_json(sm_file, videoSM_file, json_file):
+def csv_to_json(sm_file, json_file):
     sm_data = []
-    videoSM_data = []
     output_data = []
     
     # Read the CSV file
@@ -31,54 +11,36 @@ def csv_to_json(sm_file, videoSM_file, json_file):
         for row in sm_reader:
             sm_data.append(row)
     
-    with open(videoSM_file, 'r') as file2:
-        videoSM_reader = csv.DictReader(file2)
-        for row in videoSM_reader:
-            videoSM_data.append(row)
-    
     for sm_anim in sm_data:
-        for vid_anim in videoSM_data:
-            if(vid_anim['Clip Name'] == sm_anim['Clip Name']):
-                clipName = vid_anim['Clip Name']
-                startTime = int(vid_anim['From'])
-                endTime = int(vid_anim['To'])
-                parentState = sm_anim['Parents']
-                transitionTime = 0
-            
-                isEarlyExit = False
-                if(len(parentState.split('/')) > 1):
-                    parentAnimation = parentState.split('/')[1]
-                    parentState = parentState.split('/')[0]
-                    isEarlyExit = True
-
-                    parentStateSM = getDataFromClipName(parentAnimation,sm_data)
-                    parentStateVid = getDataFromClipName(parentAnimation,videoSM_data)
-                    stateSM = getDataFromClipName(clipName,sm_data)
-                    
-                    transitionOffset = int(stateSM['Transition Frame']) - int(parentStateSM['From'])
-                    #transitionOffset *= 2
-                    transitionTime = int(parentStateVid['From']) + transitionOffset
-
-                else:
-                    parentAnimation = "None"
-
-                
-                if(sm_anim['IsActive'] == "TRUE"):
-                    isActive = True
-                else: isActive = False
-
-                position, targetPosition = calcPosition(parentState)
-                
-                output_data.append({'name':clipName, 
-                                    'startTime':startTime, 
-                                    'endTime':endTime,
-                                    'transitionFromParent': transitionTime,
-                                    'parentAnimation' : parentAnimation,
-                                    'parentState':parentState,
-                                    'position':position,
-                                    'targetPosition':targetPosition,
-                                    'isEarlyExit':isEarlyExit,
-                                    'isActive':isActive})
+        clipName = sm_anim['Clip Name']
+        startTime = int(sm_anim['From'])
+        endTime = int(sm_anim['To'])
+        parentAnimation = sm_anim['Parents']
+        transitionFrame = 0
+        isEarlyExit = False
+        if(parentAnimation != ''):
+            isEarlyExit = True
+            transitionFrame = int(sm_anim['Transition Frame'])
+        if(sm_anim['IsActive'] == "TRUE"):
+            isActive = True
+        else: 
+            isActive = False
+        
+        positions = {"PB":0, "BB":1, "RG":2}
+        position = sm_anim['State']
+        state = sm_anim['State']
+        position = positions.get(sm_anim['State'])
+        targetPosition = positions.get(sm_anim['NextState'])
+        output_data.append({'name':clipName, 
+                            'startTime':startTime, 
+                            'endTime':endTime,
+                            'transitionFromParent': transitionFrame,
+                            'parentAnimation' : parentAnimation,
+                            'parentState':state,
+                            'position':position,
+                            'targetPosition':targetPosition,
+                            'isEarlyExit':isEarlyExit,
+                            'isActive':isActive})
     # Write the data to a JSON file
     with open(json_file, 'w') as file:
         json.dump(output_data, file, indent=4)
@@ -94,4 +56,4 @@ def csv_to_json(sm_file, videoSM_file, json_file):
 # Replace 'inputcsv' and 'output.json' with the actual file names
 
 
-csv_to_json('SM.csv', 'Video_SM.csv', 'video_states.json')
+csv_to_json('SM.csv', 'video_states.json')
