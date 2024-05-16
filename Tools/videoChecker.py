ceb91518828d2857b29db88b7b8ee100bdfa596c
DHWill <will@dominicharris.studio>
2024-05-16 17:33:48 +0100

    Updated Tools

--- Tools/videoChecker.py
+++ Tools/videoChecker.py
@@ -8,11 +8,14 @@ import numpy as np
 # parses the data and assigns it to a variable called jess_dict
 
 def parseJson(file= 'video_states.json'):
-
     jsonFile = open(file)
     jsonFile = json.loads(jsonFile.read())
     return jsonFile
 
+def UpdateValueByName(jsonData, _clipName, key, value):
+    for reg in jsonData:
+        if(reg['name'] == _clipName):
+            reg[key] = value
 
 def compare_frames(video_capture, frameN1, frameN2):
     # Convert frames to grayscale
@@ -52,22 +55,23 @@ def main():
                     for segCompareTo in smData:
 
                         if((segCompareTo['position'] == position) & (segCompare['isEarlyExit'] == False)):
-                            frameComp1 = segCompare['startTime'] -1
-                            frameComp2 = segCompareTo['startTime'] -1
-                            description = "Comparing: (" + segCompare['name'] + " startTime | " + segCompareTo['name'] + " startTime)"
-                            framesToCheck.append((frameComp1, frameComp2, description))
+                            frameComp1 = (segCompare,'startTime')
+                            frameComp2 = (segCompareTo,'startTime')
+                            # description = "Comparing: (" + segCompare['name'] + " startTime | " + segCompareTo['name'] + " startTime)"
+                            framesToCheck.append((frameComp1, frameComp2))
                         
                         if(segCompareTo['targetPosition'] == position):
-                            frameComp1 = segCompare['startTime'] -1
-                            frameComp2 = segCompareTo['endTime'] -1
-                            description = "Comparing: (" + segCompare['name'] + " startTime | " + segCompareTo['name'] + " endTime)"
-                            framesToCheck.append((frameComp1, frameComp2, description))
+                            frameComp1 = (segCompare,'startTime')
+                            frameComp2 = (segCompareTo,'endTime')
+                            # description = "Comparing: (" + segCompare['name'] + " startTime | " + segCompareTo['name'] + " endTime)"
+                            framesToCheck.append((frameComp1, frameComp2))
                         
                 if(segCompare['isEarlyExit'] == True):
-                    frameComp1 = segCompare['startTime'] -1
-                    frameComp2 = segCompare['transitionFromParent'] 
-                    description = "Comparing: (" + segCompare['name'] + " startTime | " + segCompare['name'] + " transitionTime)"
-                    framesToCheck.append((frameComp1, frameComp2, description))
+
+                    frameComp1 = (segCompare , 'startTime')
+                    frameComp2 = (segCompare, 'transitionFromParent') 
+                    # description = "Comparing: (" + segCompare['name'] + " startTime | " + segCompare['name'] + " transitionTime)"
+                    framesToCheck.append((frameComp1, frameComp2))
 
                         
 
@@ -77,36 +81,73 @@ def main():
         isEditing = 0
         while True:
         # for frames in framesToCheck:
-            frameComp1, frameComp2, description = framesToCheck[index]
-            frameComp1 += frameComp1Offset
-            frameComp2 += frameComp2Offset
-            alphaLap, diff = compare_frames(video_capture, frameComp1, frameComp2)
-            # printDiff = "AbsDiff between frames at {} and {} frame: {:.2f}".format(frameComp1, frameComp2, diff)
+            frameComp1, frameComp2 = framesToCheck[index]
+            frameSeg1, frameKey1 = frameComp1
+            frameSeg2, frameKey2 = frameComp2
+            
+            frameN1 = frameSeg1[frameKey1]
+            frameN2 = frameSeg2[frameKey2]
+
+            frameN1 += frameComp1Offset
+            frameN2 += frameComp2Offset
+
+            alphaLap, diff = compare_frames(video_capture, frameN1, frameN2)
+
+            description = "Comparing: (" + str(frameSeg1['name']) + " " + frameKey1 + " | " + str(frameSeg2['name']) + " " + frameKey2 + ")"
             description += " absDiff = " + str(diff)
-            frameDesc = "RED: ( " + str(frameComp1) + "  |  " + str(frameComp2) + " ) :BLUE"
+            frameDesc = "RED: ( " + str(frameN1) + "  |  " + str(frameN2) + " ) :BLUE"
             cv2.putText(alphaLap, description, (50, 50),font, 0.5, (0, 255, 0),  2, cv2.LINE_AA) 
             cv2.putText(alphaLap, frameDesc , (50, 100),font, 0.5, (0, 255, 0),  2, cv2.LINE_AA) 
+
+            debgCol = (0, 0, 0)
+            debugFill = 1
+            if(isEditing == 1):
+                debugFill = -1
+                debgCol = (0, 0, 255)
+            elif(isEditing == 2):
+                debugFill = -1
+                debgCol = (255, 0, 0)                
+            cv2.rectangle(alphaLap, (50, 125), (100, 175), thickness=debugFill, color=debgCol)
+
             cv2.imshow("alphaLap", alphaLap)
             key = cv2.waitKey(0) & 0xFF
             if(isEditing > 0):
-                if key == 82:   #up
-                    if(isEditing == 1):
-                        frameComp1Offset += 1
-                    else:
-                        frameComp2Offset += 1
-                if key == 84:   #down
-                    if(isEditing == 1):
-                        frameComp1Offset -= 1
-                    else:
-                        frameComp2Offset -= 1
-                if (key == ord('r') or key == ord('b')):
-                    isEditing = False
+                cv2.add
                 
+                if(isEditing == 1):
+                    if key == 82:   #up
+                        frameComp1Offset += 1
+                    if key == 84:   #down
+                        frameComp1Offset -= 1
+                    if key == ord('g'):#save
+                        UpdateValueByName(smData, frameSeg1['name'], frameKey1, frameN1)
+                        frameComp1Offset == 0
+                if(isEditing == 2):
+                    if key == 82:   #up
+                        frameComp2Offset += 1
+                    if key == 84:   #down
+                        frameComp2Offset -= 1
+                    if key == ord('g'):#save
+                        UpdateValueByName(smData, frameSeg2['name'], frameKey2, frameN2)
+                        frameComp1Offset == 0
+
+                
+                if key == ord('r'):
+                    isEditing = 1
+                if key == ord('b'):
+                    isEditing = 2
+                if (key == 83 or key == 81):   #exitEdit
+                    isEditing = 0
+                    
                 if key == ord('q'):
-                    break
-                    #Add update frametime
+                # Write the data to a JSON file
+                    with open('video_states_updated.json', 'w') as file:
+                        json.dump(smData, file, indent=4)
+                        break
 
             else:
+                frameComp1Offset = 0
+                frameComp2Offset = 0
                 if key == 83:   #right
                     index += 1
                 if key == 81:   #left
